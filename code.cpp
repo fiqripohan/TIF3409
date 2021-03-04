43f218246b50b09cc714fc7e3223df3184c42b3b
fiqripohan <52327774+fiqripohan@users.noreply.github.com>
2021-03-04 10:46:47 +0700

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+
+main()
+{int a,b,c;
+ a=5; b=4
+   c=a+b;
+ cout<<"\n nilai a = "<<a;
+ cout<<"\n nilai b = "<<b;
+ cout<<"\n nilai c = "<<c;
+ 
+ getch();}
